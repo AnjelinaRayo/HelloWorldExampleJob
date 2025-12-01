aadcacbecf28bcef1a769c6daaf4dfe416ef1c4c
AnjelinaRayo <126065373+AnjelinaRayo@users.noreply.github.com>
2025-12-01 13:37:37 +0530

    Update Application.java

--- src/main/java/com/qascript/app/Application.java
+++ src/main/java/com/qascript/app/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public Application() {}
 
-    public static void main(String[] args){
+    //public static void main(String[] args){
         System.out.println(new Application().getMessage());
 
     }
